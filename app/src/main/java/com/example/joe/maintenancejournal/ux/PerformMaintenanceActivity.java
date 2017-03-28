75ca7df25559091b8e8d6b8cd27ebb6d09bbbaf1
Joe <joe@newborngames.com>
2017-03-27 21:51:51 -0600

    Perform maintenance update

--- app/src/main/java/com/example/joe/maintenancejournal/ux/PerformMaintenanceActivity.java
+++ app/src/main/java/com/example/joe/maintenancejournal/ux/PerformMaintenanceActivity.java
@@ -1,7 +1,6 @@
 package com.example.joe.maintenancejournal.ux;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import com.example.joe.maintenancejournal.R;
